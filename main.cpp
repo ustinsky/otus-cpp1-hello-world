8da1e4a0bb4380a3c89efb8466dc2f0714e42989
lex <lex@mapha.org>
2019-09-09 01:19:23 +0300

    First commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "lib.h"
+
+#include <iostream>
+
+int main (int, char **)
+{
+    std::cout << "Hello !!!" << std::endl;
+    std::cout << "Version: " << version() << std::endl;
+
+    return 0;
+}
