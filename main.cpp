a8854f5a3997ee25789074276ab224bbabdbca58
lex <lex@mapha.org>
2019-09-09 23:23:10 +0300

    Проба пера 0.0.5

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 
 int main (int, char **)
 {
-    std::cout << "Hello !!!" << std::endl;
+    std::cout << "Hello, World !!!" << std::endl;
     std::cout << "Version: " << version() << std::endl;
 
     return 0;
